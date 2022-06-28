9a4174b4940e50c8c5f61895ab9adc3a59fd7966
João Levy Cristian Dos Anjos <jolevy.cristian@gmail.com>
2022-06-28 15:02:59 -0300

    add back4app support

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import Library from './views/library';
 import Map from './views/map';
 import Menu from './components/menu';
 import Profile from './views/profile';
+import Parse from 'parse/dist/parse.min.js';
+
+const PARSE_APPLICATION_ID = 'MV1X3atOq3MlMcS0yWSGCz0yum4SC5rCKaACgaLv';
+const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
+const PARSE_JAVASCRIPT_KEY = 'PmeztcfiG2GrvqHYzaddh5kJXzIappxCnOj1PULu';
+Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
+Parse.serverURL = PARSE_HOST_URL;
 
 function App() {
   return (
--- src/views/library.js
+++ src/views/library.js
@@ -1,14 +1,43 @@
 import '../style/library.css';
 import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined';
+import Parse from 'parse/dist/parse.min.js';
+import React, { useState } from 'react';
+import { Button } from '@mui/material';
+import {Image} from  'react';
 
-export default function Library(){
+
+export default function Library() {
+
+    const [posts, setPosts] = useState(null);
+
+    async function fetchPost() {
+        const query = new Parse.Query('Post');
+        let queryResult = await query.findAll();
+        console.log('posts: ', queryResult);
+
+        setPosts(queryResult);
+    }
+
     return(
         <div>
             <div className="header">
                 <label>Biblioteca Hecate</label>
-                
+            </div>
+            <div className='body'>
+                <Button variant="contained" onClick={fetchPost}>Contained</Button>
+                {posts !== null && (
+                posts.map((post, i) => { 
+                    return(
+                        <div>
+                            <p>{`Name: ${post.get('title')}`}</p>
+                            <p>{`Autor: ${post.get('author')}`}</p>
+                            <img src={post.get('cover')._url }/>
+                        </div>
+                    );
+                })
+            )}
             </div>
         </div>
     );
-
 }
+  
