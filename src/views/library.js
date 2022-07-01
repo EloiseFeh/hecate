d2a7140f8bf4592424e008b93a3af3b5acfbd831
unknown <eloiseferreira045@gmail.com>
2022-07-01 01:04:00 -0300

    fix library page and components

--- src/views/library.js
+++ src/views/library.js
@@ -1,41 +1,60 @@
-import '../style/library.css';
-import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined';
-import Parse from 'parse/dist/parse.min.js';
-import React, { useState } from 'react';
-import { Button } from '@mui/material';
+import "../style/library.css";
+// import FilterAltOutlinedIcon from '@mui/icons-material/FilterAltOutlined';
+import Parse from "parse/dist/parse.min.js";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 
 export default function Library() {
+  const [posts, setPosts] = useState(null);
 
-    const [posts, setPosts] = useState(null);
+  async function getPosts() {
+    const query = new Parse.Query("Post");
+    let queryResult = await query.findAll();
+    console.log("posts: ", queryResult);
 
-    async function fetchPost() {
-        const query = new Parse.Query('Post');
-        let queryResult = await query.findAll();
-        console.log('posts: ', queryResult);
+    setPosts(queryResult);
+  }
 
-        setPosts(queryResult);
-    }
+  useEffect(() => {
+    getPosts();
+  }, []);
 
-    return(
-        <div>
-            <div className="header">
-                <label>Biblioteca Hecate</label>
-            </div>
-            <div className='body'>
-                <Button variant="contained" onClick={fetchPost}>Contained</Button>
-                {posts !== null && (
-                posts.map((post, i) => { 
-                    return(
-                        <div>
-                            <p>{`Name: ${post.get('title')}`}</p>
-                            <p>{`Autor: ${post.get('author')}`}</p>
-                            <img src={post.get('cover')._url }/>
-                        </div>
-                    );
-                })
-            )}
-            </div>
-        </div>
-    );
+  console.log(setPosts);
+
+  return (
+    <div className="body-container">
+      <div className="header">
+        <label>Biblioteca Hecate</label>
+      </div>
+      <div className="container">
+        {posts !== null &&
+          posts.map((post, i) => {
+            return (
+              <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={post.get("cover")._url}
+                  alt="post image"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {`Name: ${post.get("title")}`}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {`Autor: ${post.get("author")}`}
+                  </Typography>
+                </CardContent>
+              </Card>
+            );
+          })}
+      </div>
+    </div>
+  );
 }
-  
