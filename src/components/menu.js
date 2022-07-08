cd7b231176435b3bddba7ae5ecb832b4833675cb
unknown <eloiseferreira045@gmail.com>
2022-07-07 21:37:37 -0300

    merge and fix conflicts

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,29 +1,30 @@
-import * as React from 'react';
+import * as React from "react";
 import { Link } from "react-router-dom";
-import '../style/menu.css';
-import NewspaperIcon from '@mui/icons-material/Newspaper';
-import PersonIcon from '@mui/icons-material/Person';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import "../style/menu.css";
+import NewspaperIcon from "@mui/icons-material/Newspaper";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
 
 export default function Menu() {
-
   return (
-    <div className='navbar'>
-      <ul className='navbar-nav'>
-        <li className='nav-item'>
-          <NewspaperIcon/>
-          <Link to="/" className='nav-link'>Home</Link>
+    <div className="menu">
+      <ul className="menu-nav">
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            <NewspaperIcon />
+          </Link>
         </li>
-        <li className='nav-item'>
-          <LocationOnIcon/>
-          <Link to="/mapa" className='nav-link'>Mapa</Link>
+        <li className="nav-item">
+          <Link to="/mapa" className="nav-link">
+            <LocationOnOutlinedIcon />
+          </Link>
         </li>
-        <li className='nav-item'>
-          <PersonIcon/>
-          <Link to="/perfil" className='nav-link'>Perfil</Link>
+        <li className="nav-item">
+          <Link to="/perfil" className="nav-link">
+            <PersonOutlineOutlinedIcon />
+          </Link>
         </li>
       </ul>
-
     </div>
   );
 }
--- src/views/map.js
+++ src/views/map.js
@@ -1,39 +1,45 @@
-import '../style/map.css';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import Parse from 'parse/dist/parse.min.js';
-import React, { useState } from 'react';
-
+import "../style/map.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import Parse from "parse/dist/parse.min.js";
+import React, { useState, useEffect } from "react";
 
 export default function Map() {
+  const [supportPoints, setSupportPoints] = useState(null);
 
-    const [supportPoints, setSupportPoints] = useState(null);
+  async function fetchSupportPoints() {
+    const query = new Parse.Query("SupportPoints");
+    let queryResult = await query.findAll();
+    console.log("SupportPoints: ", queryResult);
+    setSupportPoints(queryResult);
+  }
 
-    async function fetchSupportPoints() {
-        const query = new Parse.Query('SupportPoints');
-        let queryResult = await query.findAll();
-        console.log('SupportPoints: ', queryResult);
-        setSupportPoints(queryResult);
-    }
+  useEffect(() => {
+    fetchSupportPoints();
+  }, []);
 
-    return (
-        <div>
-            <div className="header">
-                <label>Mapa</label>
-            </div>
-            <div className='row'>
-                <MapContainer center={[51.505, -0.09]} zoom={20} scrollWheelZoom={false} className='mapView'>
-                    <TileLayer
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    <Marker position={[51.505, -0.09]}>
-                        <Popup>
-                            A pretty CSS3 popup. <br /> Easily customizable.
-                        </Popup>
-                    </Marker>
-                </MapContainer>
-            </div>
-        </div>
-    );
-
+  return (
+    <div>
+      <div className="header">
+        <label>Mapa</label>
+      </div>
+      <div className="row">
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={20}
+          scrollWheelZoom={false}
+          className="mapView"
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+    </div>
+  );
 }
